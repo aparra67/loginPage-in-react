c6dd405cc48385a63a00cc9187355ed01922d307
aparra19 <angelparra67@gmail.com>
2023-02-24 16:05:40 -0400

    Anexo de componente

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Formulario } from './Formulario';
+import './stylesheets/style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<h1>Hola Mundo</h1>)
+root.render(<div className='container'>
+    <Formulario />
+</div>)
